3b071dbad7f8c8e68cec1f6b9b1026c4a2541384
Ecanno8 <ethancannon92@gmail.com>
2023-09-30 04:07:43 -0700

    Took time away 1.6 clean slate.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-let pokemonRepository = (function () {
-    let pokemonlist = [
+var pokemonRepository = (function () {
+    let pokemonList = [
         {
             name: 'Charizard',
             types: ['Fire'],
@@ -25,59 +25,42 @@ let pokemonRepository = (function () {
             types: ['Grass'],
             height: 2,
         }
-    ]
-
-    function getAll() {
-        return pokemonList;
-    }
+    ];
 
     function add(pokemon)
-    if (typeof pokemon === 'object' &&
+    if
+        (typeof pokemon === 'object' &&
         "name" in pokemon &&
         "types" in pokemon &&
         "height" in pokemon) {
         pokemonList.push(pokemon);
     } else { console.log("pokemon is not correct"); }
 
-    function addv(pokemon) {
-        let expectedKeys = ['name', 'height', 'types'];
-        let actualKeys = Object.keys(pokemon);
-        if (typeof pokemon === 'object' && actualKeys.length === expectedKeys.length && expectedKeys.every(index => {
-            actualKeys.includes(index)
-        })) { pokemonList.push(pokemon); }
-
-        else { console.log("Invalid data.") }
-    }
-    function findPokemonByName(minLength) {
-        return pokemonRepository.getAll().filter(pokemon => pokemon.name.length > minlength);
+    function getAll() {
+        return pokemonList;
     }
 
     return {
         add: add,
         addv: addv,
         getAll: getAll,
-        findPokemonByName: findPokemonByName,
-    }
+    };
 })();
 
 
-
-
-
-//change this to a for each statement//
-for (let i = 0; i < pokemonlist.length; i++) {
-    if (pokemonlist[i].height < 2.5 && pokemonlist[i].height > 1.5) {
-        document.write(pokemonlist[i].name + pokemonlist[i].height + " Wow that's big!");
-    } else if (pokemonlist[i].height < .5) {
-        document.write(pokemonlist[i].name + pokemonlist[i].height + " It's so cute!");
-    } else {
-        document.write(pokemonlist[i].name + pokemonlist[i].height + " Congratulations!");
-    }
-};
-
 //work under for each and get all loop array//
 console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
 
-pokemonRepository.getAll().forEach(function (item, index) {
-    document.write('<p> ${pokemon.name}');
-})
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    let pokemonList = document.querySelector("pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = "pokemon.name";
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+
+});
