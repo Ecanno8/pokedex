9ac5d3660b4467b1569586a73e4809b736e8f6ac
Ecanno8 <ethancannon92@gmail.com>
2023-09-30 06:48:21 -0700

    1.6 1st attempt since break.

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,79 @@
-var pokemonRepository = (function () {
-    let pokemonList = [
+let pokemonRepository = (function () {
+    let repository = [
         {
-            name: 'Charizard',
-            types: ['Fire'],
+            name: "Bulbasaur",
+            height: 0.7,
+            types: ["grass", "poison"],
+        },
+        {
+            name: "Charizard",
             height: 1.7,
+            types: ["fire", "flying"],
         },
         {
             name: "Squirtle",
-            types: ['Water'],
-            height: .4
+            height: 1,
+            types: ["water"],
         },
-        {
-            name: 'Blastoise',
-            types: ['Water'],
-            height: 1.6,
-        },
-        {
-            name: 'Bulbasaur',
-            types: ['Grass'],
-            height: .7,
-        },
-        {
-            name: 'Venasaur',
-            types: ['Grass'],
-            height: 2,
-        }
     ];
 
-    function add(pokemon)
-    if
-        (typeof pokemon === 'object' &&
-        "name" in pokemon &&
-        "types" in pokemon &&
-        "height" in pokemon) {
-        pokemonList.push(pokemon);
-    } else { console.log("pokemon is not correct"); }
+    function add(pokemon) {
+        if (
+            typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "types" in pokemon
+        ) {
+            repository.push(pokemon);
+        } else {
+            console.log("pokemon is not correct");
+        }
 
-    function getAll() {
-        return pokemonList;
+        function addv(pokemon) {
+            let expectedKeys = ['name', 'height', 'types'];
+            let actualKeys = Object.keys(pokemon);
+
+            if (typeof pokemon === 'object' && actualKeys.length === expectedKeys.length && expectedKeys.every(index => {
+                actualKeys.includes(index)
+            })) {
+                pokemonList.push(pokemon);
+            } else {
+                console.log("Invalid data. Only non empty objects with expected keys can be added");
+            }
+        }
     }
-
+    function getAll() {
+        return repository;
+    }
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+        buttonAlert(button, pokemon);
+    }
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+    function buttonAlert(button, pokemon) {
+        button.addEventListener('click', function (event) {
+            showDetails(pokemon);
+        })
+    }
     return {
         add: add,
-        addv: addv,
         getAll: getAll,
+        addListItem: addListItem
     };
 })();
 
-
-//work under for each and get all loop array//
-console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
 
 console.log(pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-    let pokemonList = document.querySelector("pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement("button");
-    button.innerText = "pokemon.name";
-    button.classList.add("button-class");
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
-
+    pokemonRepository.addListItem(pokemon);
 });
